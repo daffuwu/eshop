588423f8916d55a027d748993ae41381e33c9a49
daffuwu <dananda41661@gmail.com>
2025-02-13 12:21:19 +0700

    Fix controller

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -24,7 +24,7 @@ public class ProductController
         return "createProduct";
     }
 
-    @GetMapping("/create")
+    @PostMapping("/create")
     public String createProductPost(@ModelAttribute Product product, Model model)
     {
         service.create(product);
